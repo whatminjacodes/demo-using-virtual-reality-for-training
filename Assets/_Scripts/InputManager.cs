f18b0a2b10e06eb08af24898d08b0a69b432225a
whatminjacodes <whatminjacodes@gmail.com>
2022-01-01 21:14:05 +0200

    Added basic input

--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -1,102 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro; //--- Added for our text messages component!
+using UnityEngine.UI;
 
 public class InputManager : MonoBehaviour
 {
-    //private TextMeshPro _messages = new();
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //--- Get the text message component so we can write messages to it!
-       // _messages = GameObject.Find("DisplayMessages").GetComponent<TextMeshPro>();
-       // if (_messages != null)
-       // {
-       //     _messages.text = "Press a Button!";
-       // }
-       // else
-       // {
-         //   Debug.Log("Component not found!");
-       // }
-    }
-
-    // Update is called once per frame
+   public Text debugText;
+   
     void Update()
     {
         //--- Right Controller
         if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch))
         {
-           // _messages.text = "A Button (Down)";
+           debugText.text = "A Button (Down)";
         }
         else if (OVRInput.GetUp(OVRInput.Button.One, OVRInput.Controller.RTouch))
         {
-           // _messages.text = "A Button (Up)";
+           debugText.text = "A Button (Up)";
         }
 
         if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch))
         {
-           // _messages.text = "B Button (Down)";
+           debugText.text = "B Button (Down)";
         }
         else if (OVRInput.GetUp(OVRInput.Button.Two, OVRInput.Controller.RTouch))
         {
-          //  _messages.text = "B Button (Up)";
+            debugText.text = "B Button (Up)";
         }
 
         if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
         {
-           // _messages.text = "Right Trigger (Down)";
+           debugText.text = "Right Trigger (Down)";
         }
         else if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
         {
-           // _messages.text = "Right Trigger (Up)";
+           debugText.text = "Right Trigger (Up)";
         }
 
         if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch))
         {
-           // _messages.text = "Right Grip (Down)";
+           debugText.text = "Right Grip (Down)";
         }
         else if (OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch))
         {
-           // _messages.text = "Right Grip (Up)";
+           debugText.text = "Right Grip (Up)";
         }
 
         //--- Left Controller
         if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.LTouch))
         {
-         //   _messages.text = "X Button (Down)";
+            debugText.text = "X Button (Down)";
         }
         else if (OVRInput.GetUp(OVRInput.Button.One, OVRInput.Controller.LTouch))
         {
-           // _messages.text = "X Button (Up)";
+           debugText.text = "X Button (Up)";
         }
 
         if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.LTouch))
         {
-           // _messages.text = "Y Button (Down)";
+           debugText.text = "Y Button (Down)";
         }
         else if (OVRInput.GetUp(OVRInput.Button.Two, OVRInput.Controller.LTouch))
         {
-          //  _messages.text = "Y Button (Up)";
+            debugText.text = "Y Button (Up)";
         }
 
         if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
         {
-          //  _messages.text = "Left Trigger (Down)";
+            debugText.text = "Left Trigger (Down)";
         }
         else if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
         {
-           // _messages.text = "Left Trigger (Up)";
+           debugText.text = "Left Trigger (Up)";
         }
 
         if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.LTouch))
         {
-           // _messages.text = "Left Grip (Down)";
+           debugText.text = "Left Grip (Down)";
         }
         else if (OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.LTouch))
         {
-           // _messages.text = "Left Grip (Up)";
+           debugText.text = "Left Grip (Up)";
         }
     }
 }
