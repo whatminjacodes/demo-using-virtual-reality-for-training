5b1f6e6984eeaecdd100e27bdcd77a12359998bc
whatminjacodes <whatminjacodes@gmail.com>
2022-01-06 01:36:11 +0200

    StartVehicleTutorial code cleaned and refactored

---
+++ Assets/_Scripts/General/VRControllerStartVehicleTutorialTriggers.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using pEventBus;
+
+public class VRControllerStartVehicleTutorialTriggers : MonoBehaviour, IEventReceiver<TutorialModuleStartedEvent>
+{
+    private string currentTutorialModuleName = "";
+    private bool startButtonGrabbedEventSent = false;
+
+    /*  Unity methods   */
+    private void Start() {
+        RegisterEvents();
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if(currentTutorialModuleName == Constants.START_VEHICLE_TUTORIAL_NAME) {
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+            {
+                if(other.gameObject.name == Constants.START_BUTTON_NAME && !startButtonGrabbedEventSent) {
+
+                    EventBus<StartButtonGrabbedEvent>.Raise(new StartButtonGrabbedEvent() {});
+                    startButtonGrabbedEventSent = true;
+                }
+            } else if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+            {
+                if(other.gameObject.name == Constants.START_BUTTON_NAME && startButtonGrabbedEventSent) {
+
+                    EventBus<StartButtonLetGoEvent>.Raise(new StartButtonLetGoEvent() {});
+                    startButtonGrabbedEventSent = false;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(currentTutorialModuleName == Constants.START_VEHICLE_TUTORIAL_NAME) {
+            if(other.gameObject.name == Constants.START_BUTTON_NAME && startButtonGrabbedEventSent) {
+
+                EventBus<StartButtonLetGoEvent>.Raise(new StartButtonLetGoEvent() {});
+                startButtonGrabbedEventSent = false;
+            }
+        }
+    }
+
+    /*  Events  */
+    public void RegisterEvents() {
+        EventBus.Register(this);
+    }
+
+    public void UnRegisterEvents() {
+        EventBus.UnRegister(this);
+    }
+
+    public void OnEvent(TutorialModuleStartedEvent e) {
+        currentTutorialModuleName = e.nameOfModuleThatIsStarting;
+    }
+}
--- Assets/_Scripts/Tutorials/StartVehicleTutorial.cs
+++ Assets/_Scripts/Tutorials/StartVehicleTutorial.cs
@@ -8,112 +8,62 @@ using pEventBus;
 public class StartVehicleTutorial : MonoBehaviour, IEventReceiver<TutorialModuleStartedEvent>, IEventReceiver<StartButtonGrabbedEvent>,
 IEventReceiver<StartButtonLetGoEvent>
 {
-    public ItemInitialization _startButtonData;
-
+    [Header("Needed GameObjects")]
+    [SerializeField] private GameObject _rightController;
     [SerializeField] private GameObject _startButton;
     [SerializeField] private GameObject _startLEDWhite;
     [SerializeField] private GameObject _startLEDRed;
 
+    [Header("Needed UI elements")]
     [SerializeField] private TMP_Text _tutorialText;
     [SerializeField] private TMP_Text _debugText;
     [SerializeField] private TMP_Text _debugControllerText;
 
-    [SerializeField] private GameObject _rightController;
+    // Event checks
+    private bool tutorialStartedFromEvent = false;
+    private bool startButtonGrabbed = false;
 
-    int _numOfClicks = 1;
-    int _numberOfStepsBeforeFinished = 5;
+    // Start button rotation
+    private Quaternion initialObjectRotation;
+    private Quaternion initialControllerRotation;
+    private bool initialRotationsSet = false;
 
-    int _speed = 10;
-
-    bool tutorialStartedFromEvent = false;
-
-    private SphereCollider _startButtonCollider;
-
-    bool startButtonGrabbed = false;
-
-    Vector3 _startButtonPosition;
-
-    Vector3 _startButtonMaxRotation = new Vector3(0, -90, 0);
-
-    private Quaternion currentRot;
-    private Vector3 startPos;
-    private bool offsetSet;
-
-    private bool set = false;
-    
-  private Quaternion initialObjectRotation;
-  private Quaternion initialControllerRotation;
-
+    /*  Unity methods   */
     private void Start() {
-        RegisterEvents();
         _debugText.text = "StartVehicleTutorial: registering events at start";
+        RegisterEvents();
     }
 
-    private void Initialize() {
-        /* if(_startButton != null) {
-            // Position & rotation
-            _startButton.transform.localPosition =  new Vector3(0,0,0);
-            Quaternion defaultRotation = Quaternion.Euler(_startButtonData.defaultRotation.x, _startButtonData.defaultRotation.y, _startButtonData.defaultRotation.z);
-           _startButton.transform.localRotation = defaultRotation;
-
-            // Disable collider
-           _startButtonCollider = _startButton.GetComponent<SphereCollider>();
-        
-            if (_startButtonCollider != null) {
-                _startButtonCollider.enabled = false;
-            }
-            else {
-                Debug.LogError("Start button collider missing!");
-            }
-
-            // Set LEDs
-            _startLEDWhite.SetActive(true);
-            _startLEDRed.SetActive(false);
-        }*/
-    }
-
-    void Update()
+    private void Update()
     {
         if(tutorialStartedFromEvent == true) {
-            _startButtonPosition = _startButton.transform.position;
-
-            /* if(_numOfClicks == _numberOfStepsBeforeFinished) {
-                    _tutorialText.text = "Tutorial module finished";
-
-                    FinishAndCloseTutorial();
-                }
-                else if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch)) {
-                    //_debugControllerText.text = "Right Trigger (Up)";
-                    _tutorialText.text = "Going to " + _numOfClicks + " part.";
-                _numOfClicks++;
-            }*/
             if(startButtonGrabbed) {
-            // _startButton.transform.RotateAround(_startButton.transform.position, _startButton.transform.up, Time.deltaTime * _speed * -1f);
-            // _startButton.transform.position = _startButtonPosition;
-               //Debug.LogWarning("start rot y: " + _startButton.transform.localRotation.eulerAngles.y);
-               // Rotate();
-              // FinishAndCloseTutorial();
+                if(initialRotationsSet == false)
+                {
+                    initialObjectRotation= _startButton.transform.localRotation;
+                    initialControllerRotation = _rightController.transform.rotation;
+                    initialRotationsSet = true;
+                }
 
-                if(set == false)
-                    {
-                        initialObjectRotation= _startButton.transform.localRotation;
-                        initialControllerRotation = _rightController.transform.rotation;
-                        set = true;
-                    }
+                Quaternion controllerAngularDifference = initialControllerRotation * Quaternion.Inverse(_rightController.transform.rotation);
+                var rotationAmount = controllerAngularDifference * initialObjectRotation;
+                 _startButton.transform.localRotation = Quaternion.Inverse(Quaternion.Euler(0, rotationAmount.eulerAngles.y, 0));
+                
+                // TODO: Check when rotated enough and then set lights on and finish tutorial then
+                SetLEDLightsOn(true);
+                FinishAndCloseTutorial();
 
-                    Quaternion controllerAngularDifference = initialControllerRotation * Quaternion.Inverse(_rightController.transform.rotation);
-                    var asd = controllerAngularDifference * initialObjectRotation;
-
-                     _startButton.transform.localRotation = Quaternion.Inverse(Quaternion.Euler(0, asd.eulerAngles.y, 0));
-                }         
-            else
-            {
-                 set = false;
+            } else {
+                initialRotationsSet = false;
             }
         }
-        
     }
 
+    private void OnDestroy() {
+        UnRegisterEvents();
+    }
+
+    /*  Tutorial methods   */
     private void SetLEDLightsOn(bool vehicleStarted) {
         if(vehicleStarted) {
             _startLEDWhite.SetActive(true);
@@ -124,60 +74,6 @@ IEventReceiver<StartButtonLetGoEvent>
         }
     }
 
-    void SetOffsets()
-    {
-        /*if (offsetSet)
-            return;
- 
-        startPos = Vector3.Normalize(_rightController.transform.position - _startButton.transform.position);
-        currentRot = _startButton.transform.localRotation;
- 
-        offsetSet = true;*/
-    }
-
-    void Rotate()
-    {
-       /* SetOffsets();
- 
-        Vector3 closestPoint = Vector3.Normalize(_rightController.transform.position - _startButton.transform.position);
-        //_startButton.transform.rotation = Quaternion.FromToRotation(startPos, closestPoint) * currentRot;
-
-        var rot = Quaternion.FromToRotation(startPos, closestPoint);
-        rot = Quaternion.Euler(0, rot.eulerAngles.y, 0);
-        _startButton.transform.localRotation = rot * currentRot;*/
-
-    }
- 
-    /*  Events  */
-    public void OnEvent(TutorialModuleStartedEvent e) {
-        _debugText.text = "StartVehicleTutorial: on event, e: " + e.nameOfModuleThatIsStarting;
-        if(e.nameOfModuleThatIsStarting == this.gameObject.name) {
-            _debugText.text = "StartVehicleTutorial: on event, tutorial starting";
-            tutorialStartedFromEvent = true;
-            Initialize();
-        }
-    }
-
-    public void OnEvent(StartButtonGrabbedEvent e) {
-        _debugText.text = "StartVehicleTutorial: on event start button grabbed";
-        startButtonGrabbed = true; 
-       SetLEDLightsOn(true);
-    }
-
-    public void OnEvent(StartButtonLetGoEvent e) {
-        _debugText.text = "StartVehicleTutorial: on event start button let go";
-        startButtonGrabbed = false; 
-    }
-
-    public void RegisterEvents() {
-        EventBus.Register(this);
-    }
-
-    public void UnRegisterEvents() {
-        EventBus.UnRegister(this);
-    }
-
-    /*  Finishing tutorial */
     public void FinishAndCloseTutorial() {
         tutorialStartedFromEvent = false;
         startButtonGrabbed = false;
@@ -192,7 +88,29 @@ IEventReceiver<StartButtonLetGoEvent>
         this.gameObject.SetActive(false);
     }
 
-    private void OnDestroy() {
-        UnRegisterEvents();
+    /*  Events  */
+    public void RegisterEvents() {
+        EventBus.Register(this);
+    }
+
+    public void UnRegisterEvents() {
+        EventBus.UnRegister(this);
+    }
+
+    public void OnEvent(TutorialModuleStartedEvent e) {
+        if(e.nameOfModuleThatIsStarting == this.gameObject.name) {
+            _debugText.text = "StartVehicleTutorial: on event, tutorial starting";
+            tutorialStartedFromEvent = true;
+        }
+    }
+
+    public void OnEvent(StartButtonGrabbedEvent e) {
+        _debugText.text = "StartVehicleTutorial: on event, start button grabbed";
+        startButtonGrabbed = true; 
+    }
+
+    public void OnEvent(StartButtonLetGoEvent e) {
+        _debugText.text = "StartVehicleTutorial: on event, start button let go";
+        startButtonGrabbed = false; 
     }
 }
